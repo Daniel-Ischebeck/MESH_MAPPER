bf79da03d15ba03181931ce3e7ae01cef7a74e22
Daniel-Ischebeck <eeydi1@nottingham.ac.uk>
2023-03-05 17:39:27 +0000

    computed values for u and v

--- src/TRI_reader.cpp
+++ src/TRI_reader.cpp
@@ -218,9 +218,11 @@ int main()
     // look at index numbers of triangle, and compare these to j, if theres a match, vertex j is part of triangle i
 
     std::cout << "\n\n\n\n";
-    Eigen::MatrixXd M = Eigen::MatrixXd::Zero(10,8);
-    //would be sparse in reality, dense for now.
+    Eigen::MatrixXd M = Eigen::MatrixXd::Zero(10, 8);
+    // would be sparse in reality, dense for now.
 
+    Eigen::MatrixXd A_Mf1_pre = Eigen::MatrixXd::Zero(10, 8);
+    Eigen::MatrixXd A_Mf2_pre = Eigen::MatrixXd::Zero(10, 8);
 
     for (int i = 0; i < listOfFaces.size(); i++)
     { // loop through the triangles
@@ -228,18 +230,179 @@ int main()
         for (int j = 0; j < listOfPoints.size(); j++)
         { // loop through the points
 
-            if (listOfFaces.at(i).get_aIndex() == j ||
-                listOfFaces.at(i).get_bIndex() == j ||
-                listOfFaces.at(i).get_cIndex() == j)
-            { // if the first vertex of the triangle is equal to 0
-                std::cout << "Triangle " << i  << " contains vertex " << j << "\n";
-                M(i,j) = 1;
+            // if (listOfFaces.at(i).get_aIndex() == j ||
+            //     listOfFaces.at(i).get_bIndex() == j ||
+            //     listOfFaces.at(i).get_cIndex() == j)
+            // { // if the first vertex of the triangle is equal to 0
+            //     std::cout << "Triangle " << i  << " contains vertex " << j << "\n";
+            //     M(i,j) = 1;
+            // }
+            if (listOfFaces.at(i).get_aIndex() == j) // Weight 1
+            {
+                // real part x3-x2
+                A_Mf1_pre(i, j) = (listOfPoints.at(listOfFaces.at(i).get_cIndex()).get_x() -
+                                   listOfPoints.at(listOfFaces.at(i).get_bIndex()).get_x()) /
+                                  sqrt(abs(listOfAreas.at(i))); // W1/sqrt(dt)       //W1 = (x3-x2)+ i (y3-y2)
+                                                                // because dividing by sqrt of negative, should this be in complex part?
+
+                // complex part y3-y2
+                A_Mf2_pre(i, j) = (listOfPoints.at(listOfFaces.at(i).get_cIndex()).get_y() -
+                                   listOfPoints.at(listOfFaces.at(i).get_bIndex()).get_y()) /
+                                  sqrt(abs(listOfAreas.at(i))); // W1/sqrt(dt)       //W1 = (x3-x2)+ i (y3-y2)
+                                                                // because dividing by sqrt of negative, should this be in complex part?
+            }
+
+            if (listOfFaces.at(i).get_bIndex() == j) // Weight 2
+            {
+                // real part x1-x2
+                A_Mf1_pre(i, j) = (listOfPoints.at(listOfFaces.at(i).get_aIndex()).get_x() -
+                                   listOfPoints.at(listOfFaces.at(i).get_bIndex()).get_x()) /
+                                  sqrt(abs(listOfAreas.at(i)));
+
+                // complex part y1-y3
+                A_Mf2_pre(i, j) = (listOfPoints.at(listOfFaces.at(i).get_aIndex()).get_y() -
+                                   listOfPoints.at(listOfFaces.at(i).get_cIndex()).get_y()) /
+                                  sqrt(abs(listOfAreas.at(i)));
+            }
+
+            if (listOfFaces.at(i).get_bIndex() == j) // Weight 3
+            {
+                // real part x2-x1
+                A_Mf1_pre(i, j) = (listOfPoints.at(listOfFaces.at(i).get_bIndex()).get_x() -
+                                   listOfPoints.at(listOfFaces.at(i).get_aIndex()).get_x()) /
+                                  sqrt(abs(listOfAreas.at(i)));
+
+                // complex part y2-y1
+                A_Mf2_pre(i, j) = (listOfPoints.at(listOfFaces.at(i).get_bIndex()).get_y() -
+                                   listOfPoints.at(listOfFaces.at(i).get_aIndex()).get_y()) /
+                                  sqrt(abs(listOfAreas.at(i)));
             }
         }
     }
 
-    std::cout << "\n\nFilled matrix M:\n\n";
-    std:: cout << M << "\n\n" << std::endl;
+    // std::cout << "\n\nFilled matrix M:\n\n";
+    // std::cout << M << "\n\n"
+    //           << std::endl;
+
+    std::cout << "\n\nA_Mf1:\n\n";
+    std::cout << A_Mf1_pre << "\n\n"
+              << std::endl;
+
+    std::cout << "\n\nA_Mf2:\n\n";
+    std::cout << A_Mf2_pre << "\n\n"
+              << std::endl;
+
+    // we want to copy pinned coordinate data to its own thing, remove these from the matrix
+    // and resize the matrix
+
+    // pinning verticies 4 and 6
+    // which are the 3th and 5th column in the matrix
+    Eigen::MatrixXd b_Mp1 = Eigen::MatrixXd::Zero(10, 2);
+    Eigen::MatrixXd b_Mp2 = Eigen::MatrixXd::Zero(10, 2);
+
+    b_Mp1 << A_Mf1_pre.block<10, 1>(0, 3), A_Mf1_pre.block<10, 1>(0, 5); // concatting the two columns for pinned matrix
+    std::cout << "\n\nb_Mp1:\n\n";
+    std::cout << b_Mp1 << "\n\n"
+              << std::endl;
+
+    b_Mp2 << A_Mf2_pre.block<10, 1>(0, 3), A_Mf2_pre.block<10, 1>(0, 5); // concatting the two columns for pinned matrix
+    std::cout << "\n\nb_Mp2:\n\n";
+    std::cout << b_Mp2 << "\n\n"
+              << std::endl;
+
+    // now to remove the columns from matrix
+
+    unsigned int colToRemove = 3;
+    unsigned int numRows = A_Mf1_pre.rows();
+    unsigned int numCols = A_Mf1_pre.cols() - 1;
+
+    if (colToRemove < numCols)
+    {
+        A_Mf1_pre.block(0, colToRemove, numRows, numCols - colToRemove) = A_Mf1_pre.block(0, colToRemove + 1, numRows, numCols - colToRemove);
+        A_Mf2_pre.block(0, colToRemove, numRows, numCols - colToRemove) = A_Mf2_pre.block(0, colToRemove + 1, numRows, numCols - colToRemove);
+    }
+    A_Mf1_pre.conservativeResize(numRows, numCols);
+    A_Mf2_pre.conservativeResize(numRows, numCols);
+
+    colToRemove = 4; // as weve removed one, to get what was 6th element, (now 5th), i.e. index 4
+    numRows = A_Mf1_pre.rows();
+    numCols = A_Mf1_pre.cols() - 1;
+    if (colToRemove < numCols)
+    {
+        A_Mf1_pre.block(0, colToRemove, numRows, numCols - colToRemove) = A_Mf1_pre.block(0, colToRemove + 1, numRows, numCols - colToRemove);
+        A_Mf2_pre.block(0, colToRemove, numRows, numCols - colToRemove) = A_Mf2_pre.block(0, colToRemove + 1, numRows, numCols - colToRemove);
+    }
+    A_Mf1_pre.conservativeResize(numRows, numCols);
+    A_Mf2_pre.conservativeResize(numRows, numCols);
+
+    std::cout << "\n\nRemoved some stuff...A_Mf1:\n\n";
+    std::cout << A_Mf1_pre << "\n\n"
+              << std::endl;
+
+    std::cout << "\n\nRemoved some stuff...A_Mf2:\n\n";
+    std::cout << A_Mf2_pre << "\n\n"
+              << std::endl;
+
+    /*
+        //testing complex number format
+        std::complex<double> c(1, 1);
+        Eigen::MatrixXd R1;
+        R1.setRandom(2, 2);
+        Eigen::MatrixXcd C1 = c * R1; // multiply complex*real
+        Eigen::MatrixXcd C2 = c * C1; // complex scalar times complex matrix
+        C1(0, 0) = c;                 // assign complex value.
+            std::cout << C1 << "\n\n"
+                  << std::endl;
+    */
+
+    // Form A, consists of 4 block matricies
+    /*
+    Mf1 -Mf2
+    Mf2 Mf1
+    */
+    Eigen::MatrixXd A_top = Eigen::MatrixXd::Zero(10, 12);
+    Eigen::MatrixXd A_bottom = Eigen::MatrixXd::Zero(10, 12);
+    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(20, 12);
+
+    A_top << A_Mf1_pre, -A_Mf2_pre;
+    A_bottom << A_Mf2_pre, A_Mf1_pre;
+
+    A << A_top, A_bottom;
+
+    std::cout << A << "\n\n"
+              << std::endl;
+
+
+
+    Eigen::MatrixXd Bmat_top = Eigen::MatrixXd::Zero(10, 4);
+    Eigen::MatrixXd Bmat_bottom = Eigen::MatrixXd::Zero(10, 4);
+    Eigen::MatrixXd Bmat = Eigen::MatrixXd::Zero(20, 4);
+
+    Bmat_top << b_Mp1, -b_Mp2;
+    Bmat_bottom << b_Mp2, b_Mp1;
+
+    Bmat << Bmat_top, Bmat_bottom;
+
+    //b = - Bmat x PinnedVector          ??
+    //do we assign values for Up1??
+
+    std::cout << Bmat << "\n\n"
+              << std::endl;
+
+
+    Eigen::VectorXd pinned(4,1);
+    pinned << 0,1,0,0;    //choosing to pin in UV space, one coord at (1,1) other at (10,10)
+
+
+    Eigen::MatrixXd RHS = -Bmat * pinned;
+
+    std::cout << RHS << "\n\n" << std::endl;
+
+    std::cout << "The least-squares solution is:\n"
+        //<< A.template bdcSvd<Eigen::ComputeThinU | Eigen::ComputeThinV>().solve(Bmat) << std::endl;
+         << A.colPivHouseholderQr().solve(RHS); 
+
+
 
     return 0;
 }
